d09faeab5f3ba43aee95d59dad1a37142cb727b8
dtyrrelldev <davidtyrrell2022@gmail.com>
2026-06-04 14:27:48 -0600

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        # build prefix array:
+        prefix = []
+        suffix = []
+        pre = -1
+
+        for num in height:
+            if(num > pre):
+                prefix.append(num)
+                pre = num
+            else:
+                prefix.append(pre)
+
+        # build suffix array:
+        suff = -1
+        for num in reversed(height):
+            if (num > suff):
+                suffix.append(num)
+                suff = num 
+            else:
+                suffix.append(suff)
+        
+        suffix.reverse()
+
+        # then iterate thru heights w formula and prefix/suffix arrays, use formula to find water trapped:
+        area = 0
+
+        for i in range(len(height) - 1):
+            area += min(prefix[i], suffix[i]) - height[i]
+
+
+        return area
